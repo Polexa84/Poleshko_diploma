b2549217fafc57a3eff66b34fc52b4f2ecabfe4f
polexa84 <polexa84@gmail.com>
2025-07-24 21:29:42 +0300

    обновлен диплой

--- booking_system/settings.py
+++ booking_system/settings.py
@@ -6,8 +6,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
-
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = []
