9c3ac5cd2574344bfc1a84bc1e3ce78287c2b446
polexa84 <polexa84@gmail.com>
2025-07-25 14:19:30 +0300

    обновлены модели

--- bookings/models.py
+++ bookings/models.py
@@ -1,18 +1,46 @@
 import uuid
 from django.db import models
 from django.utils import timezone
+from django.conf import settings
 from tables.models import Table
 from restaurants.models import Restaurant
-from django.contrib.auth.models import User
 
 class Booking(models.Model):
-    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, verbose_name='Ресторан')
-    table = models.ForeignKey(Table, on_delete=models.CASCADE, related_name='bookings', verbose_name='Стол')
-    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь', blank=True, null=True)
-    booking_time = models.DateTimeField(verbose_name='Время бронирования')
-    duration = models.DurationField(default=timezone.timedelta(hours=3), verbose_name='Длительность')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Создано')
-    confirmation_token = models.UUIDField(default=uuid.uuid4, editable=False, unique=True) # Unique confirmation token
+    restaurant = models.ForeignKey(
+        Restaurant,
+        on_delete=models.CASCADE,
+        verbose_name='Ресторан'
+    )
+    table = models.ForeignKey(
+        Table,
+        on_delete=models.CASCADE,
+        related_name='bookings',
+        verbose_name='Стол'
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,  # Исправлено здесь!
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь',
+        blank=True,
+        null=True
+    )
+    booking_time = models.DateTimeField(
+        verbose_name='Время бронирования',
+        default=timezone.now  # Добавлено значение по умолчанию
+    )
+    duration = models.DurationField(
+        default=timezone.timedelta(hours=3),
+        verbose_name='Длительность'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Создано'
+    )
+    confirmation_token = models.UUIDField(
+        default=uuid.uuid4,
+        editable=False,
+        unique=True
+    )
     is_confirmed = models.BooleanField(default=False)
 
     def __str__(self):
@@ -21,3 +49,4 @@ class Booking(models.Model):
     class Meta:
         verbose_name = 'Бронь'
         verbose_name_plural = 'Брони'
+        ordering = ['-booking_time']  # Добавлена сортировка по умолчанию
